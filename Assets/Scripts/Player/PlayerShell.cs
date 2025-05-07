3a80af497539548c5b23854143882d5f568e870e
yowel0 <yoel.koolwaaij@gmail.com>
2025-05-07 16:40:01 +0200

    controls

--- Assets/Scripts/Player/PlayerShell.cs
+++ Assets/Scripts/Player/PlayerShell.cs
@@ -1,19 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerShell : MonoBehaviour
 {
+    public enum ControlScheme{
+        Menu,
+        Movement
+    };
+    public ControlScheme controlScheme = ControlScheme.Menu;
     public string userName = "a";
     // Start is called before the first frame update
     void Start()
     {
-        
+        GetComponent<PlayerInput>().SwitchCurrentActionMap("Menu");
+        print(GetComponent<PlayerInput>().currentActionMap);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void SwitchControlScheme(ControlScheme _controlScheme){
+        controlScheme = _controlScheme;
+        switch (controlScheme){
+            case ControlScheme.Menu:
+                break;
+            case ControlScheme.Movement:
+                break;
+        };
     }
 }
