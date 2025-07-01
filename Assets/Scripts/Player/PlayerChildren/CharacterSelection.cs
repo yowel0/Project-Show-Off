a2de197bb51e0c883156a32d56443f48fe01ad79
yowel0 <yoel.koolwaaij@gmail.com>
2025-07-01 13:19:16 +0200

    player tag UI

--- Assets/Scripts/Player/PlayerChildren/CharacterSelection.cs
+++ Assets/Scripts/Player/PlayerChildren/CharacterSelection.cs
@@ -73,6 +73,10 @@ public class CharacterSelection : MonoBehaviour
         ShowHat(selectedHat);
         SelectButton(firstSelected);
         SetEditingComponent(editingComponent);
+
+        //set name tag color
+        Animator animator = transform.Find("Name Tag").GetComponent<Animator>();
+        animator.SetInteger("playerID",PlayerManager.Instance.GetPlayerID(playerShell));
     }
 
     void SelectButton(Button button) {
